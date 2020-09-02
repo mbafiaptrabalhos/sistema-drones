4bb53f7bdfe0cb65a674e03f3074b3505902468b
Jonathas Garcia <jonathas-garcia>
2020-09-02 10:24:05 -0300

    Request mapping no plural

--- kafka-microsservice-consumer-b/src/main/java/br/com/fiap/sistema/drones/controller/DroneInfoController.java
+++ kafka-microsservice-consumer-b/src/main/java/br/com/fiap/sistema/drones/controller/DroneInfoController.java
@@ -7,23 +7,25 @@ import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import br.com.fiap.sistema.drones.model.DroneInfo;
 import br.com.fiap.sistema.drones.service.DroneInfoService;
 
 @RestController
+@RequestMapping("drones")
 public class DroneInfoController {
 
 	@Autowired
 	private DroneInfoService service;
 
-	@GetMapping("/drone")
+	@GetMapping
 	public ResponseEntity<List<DroneInfo>> getAllDrones() {
 		return new ResponseEntity<>(service.getAllDrones(), HttpStatus.OK);
 	}
 
-	@GetMapping("/drone/{id}")
+	@GetMapping("/{id}")
 	public ResponseEntity<DroneInfo> getById(@PathVariable String id) {
 		return new ResponseEntity<>(service.getById(id), HttpStatus.OK);
 	}
