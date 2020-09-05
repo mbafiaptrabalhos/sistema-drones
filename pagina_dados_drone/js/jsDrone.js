4ee24643bd306be6e5b7304744b9eb5f34ddf35c
Caroline <caroline.n.oliveira@outlook.com>
2020-09-04 23:05:39 -0300

    Pagina cadastro drone

---
+++ pagina_dados_drone/js/jsDrone.js
@@ -0,0 +1,8 @@
+document.getElementById("sliderTemperatura").oninput = function() {
+  document.getElementById("valueTemperatura").innerHTML = this.value;
+}
+
+document.getElementById("sliderUmidade").oninput = function() {
+  document.getElementById("valueUmidade").innerHTML = this.value;
+}
+
