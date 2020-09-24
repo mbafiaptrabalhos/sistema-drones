cdd6ecc71267c4ac98bca5f1784e34599019917e
Victor Tokuto Kiam <peo_vkiam@uolinc.com>
2020-09-24 11:08:56 -0300

    mapa drone

--- pagina_dados_drone/README.md
+++
@@ -1,1 +0,0 @@
-# Pagina dados drone
--- pagina_dados_drone/js/jsDrone.js
+++
@@ -1,8 +0,0 @@
-document.getElementById("sliderTemperatura").oninput = function() {
-  document.getElementById("valueTemperatura").innerHTML = this.value;
-}
-
-document.getElementById("sliderUmidade").oninput = function() {
-  document.getElementById("valueUmidade").innerHTML = this.value;
-}
-
