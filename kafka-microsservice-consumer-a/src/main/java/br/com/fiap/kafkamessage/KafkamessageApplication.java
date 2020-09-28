e18b24e7350930d6e879a450bb1d64a2180ab490
renato-brito <60199809+renato-brito@users.noreply.github.com>
2020-09-27 23:57:48 -0300

    initial

---
+++ kafka-microsservice-consumer-a/src/main/java/br/com/fiap/kafkamessage/KafkamessageApplication.java
@@ -0,0 +1,13 @@
+package br.com.fiap.kafkamessage;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KafkamessageApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(KafkamessageApplication.class, args);
+	}
+
+}
